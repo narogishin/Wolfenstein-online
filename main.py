9872642eb4251bb28884dd579a914850e9e62cad
narogishin <tdoctor109@gmail.com>
2023-12-20 23:32:59 +0100

    modified:   main.py 	modified:   map.py 	modified:   settings.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame as pg
 import sys
 from settings import RESOLUTION, FPS
+from map import Map
 
 class Game:
   def __init__(self) -> None:
     pg.init()
     self.screen = pg.display.set_mode(RESOLUTION)
     self.clock = pg.time.Clock()
+    self.new_game()
 
   def new_game(self):
-    pass
+   self.map = Map(self)
 
   def update(self):
     pg.display.flip()
@@ -18,6 +20,7 @@ class Game:
 
   def draw(self):
     self.screen.fill('black')
+    self.map.draw()
 
   def check_event(self):
     for event in pg.event.get():
